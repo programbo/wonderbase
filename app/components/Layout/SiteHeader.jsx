bc8ea7fb80e44d4717ee21b39be79efb1697128d
John Lombardo <john@programbo.com>
2016-05-22 15:01:01 +0800

    Restore side menu

--- app/components/Layout/SiteHeader.jsx
+++ app/components/Layout/SiteHeader.jsx
@@ -1,29 +1,16 @@
 import React from 'react';
-import { IndexLink, Link } from 'react-router';
-import { Navbar, NavbarHeader, NavbarCollapse, NavbarToggle, NavbarBrand } from '../Navbar';
-
-const logo = typeof window !== 'undefined' ? require('../../images/logo.png') : 'images/logo.png';
+import Logo from '../Logo';
+import MainMenu from '../MainMenu';
 
 const SiteHeader = () => (
-  <Navbar className="mainmenu">
-    <NavbarHeader>
-      <NavbarToggle menuId="mainmenu-links"/>
-      <NavbarBrand logo={logo} menuId="mainmenu-links">
-        XXX
-      </NavbarBrand>
-    </NavbarHeader>
-    <NavbarCollapse align="right" menuId="mainmenu-links">
-      <li>
-        <IndexLink to="/">Home</IndexLink>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </NavbarCollapse>
-  </Navbar>
+  <header className="site-header">
+    <div className="site-header-content">
+      <MainMenu/>
+      <div className="logos">
+        <Logo>XXX</Logo>
+      </div>
+    </div>
+  </header>
 );
 
 export default SiteHeader;
